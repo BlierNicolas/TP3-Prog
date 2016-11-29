3fc463ba34f148bc4cb04494ef6a090287a16765
Defsv <defsv8@gmail.com>
2016-11-29 15:39:15 -0500

    Update AI

--- src/bn/JoueurArtificiel.java
+++ src/bn/JoueurArtificiel.java
@@ -2,28 +2,44 @@ package bn;
 
 public class JoueurArtificiel extends AJoueur {
 	
-	boolean isSearching;
-	boolean isAttacking;
+	boolean searchState;
+	boolean isNormal;
+	boolean attackState;
+	private Position[] nextTire;//Ajouter File
 	JoueurArtificiel(){
 		super(); 
-		isSearching = true;
+		searchState = true;
+		attackState = false;
+		isNormal = false;
 	}
 
 	@Override
 	public void tire(IJoueur cible) {
-		// TODO Auto-generated method stub
-		if(isSearching && !isAttacking){
+		if(searchState && !attackState){
 			Position posTirer = cible.getRandPositionGrille();	
 			cible.recoitTire(posTirer);
 			
 			//S'il a toucher un navire
 			if(cible.flotte.getGrille()[posTirer.getColonne()][posTirer.getRangee()].getEstOccuper()){
-				isAttacking = true;
-			
+				attackState = true;
+				setNextTire(posTirer);
 			}
+		}
+		if(isNormal && !searchState){
+			if(nextTire[0] != null){
+				cible.recoitTire(nextTire[0]);
+			}else{
+				searchState = true;
+			}
+			
 		}
 		
 		//Ajout stat
+	}
+	
+	private void setIsNormal(boolean _value){
+		this.isNormal = _value;
+				
 	}
 
 	@Override
@@ -33,5 +49,36 @@ public class JoueurArtificiel extends AJoueur {
 		position.setTouche(true);
 		
 	}
+	
+	private void setNextTire(Position _position){
+		//Ajouter NORD/SUD/EST/OUEST de _position dans le tableau NextTire
+		
+		Position Nord = new Position(_position.getColonne(), _position.getRangee() + 1);
+		Position Sud =  new Position(_position.getColonne(), _position.getRangee() - 1);
+		Position Ouest =  new Position(_position.getColonne() - 1, _position.getRangee());
+		Position Est =  new Position(_position.getColonne() + 1, _position.getRangee());
+		
+		/*
+		 * Ajouter les positions dans NextTire
+		 */
+		
+		validationNextTire();
+	}
+	
+	private void validationNextTire(){
+		for(int i = 0; i< nextTire.length; i++){
+			if(nextTire[i].getColonne() > 0 && nextTire[i].getColonne() < MAX_RANGEE_COLONNE && 
+					nextTire[i].getRangee() > 0 && nextTire[i].getRangee() < MAX_RANGEE_COLONNE){
+					//Position est bonne
+					//Position doit pas etre tirer
+				
+			}else{
+				//Remove Position from the list
+				nextTire[i] = null;
+			}
+			
+		}
+	}
+	
 
 }
