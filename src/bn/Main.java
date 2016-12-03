c802b056394f279f5669409c3799f3abca57abb1
Nicolas Blier <nicolasblier@hotmail.com>
2016-12-03 00:48:57 -0500

    Fix d'une partie du frame

--- src/bn/Main.java
+++ src/bn/Main.java
@@ -1,7 +1,17 @@
 package bn;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTable;
+import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
+
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.GridLayout;
+
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 
 public class Main {
@@ -49,6 +59,62 @@ public class Main {
 	}
 	
 	public static void demarre() {
-		Frame test = new Frame("Bataille navalle");
+		JFrame framePrincipal = new JFrame("Bataille navalle");
+		framePrincipal.setBounds(200, 200, 800, 600);
+		framePrincipal.setLayout(new BorderLayout());
+				
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.Y_AXIS));
+		framePrincipal.add(buttonPanel, BorderLayout.LINE_START);
+		buttonPanel.add(addButton("Placer Porte avion", buttonPanel));
+		buttonPanel.add(addButton("Placer Croisseur", buttonPanel));
+		buttonPanel.add(addButton("Placer Contre-T", buttonPanel));
+		buttonPanel.add(addButton("Placer Sous-Marin", buttonPanel));
+		buttonPanel.add(addButton("Placer Torpilleur", buttonPanel));
+		buttonPanel.add(addButton("Annuler", buttonPanel));
+		buttonPanel.add(addButton("Aide", buttonPanel));
+		buttonPanel.add(addButton("Recommencer", buttonPanel));
+		
+		JPanel grillePanel = new JPanel();
+		grillePanel.setLayout(new GridLayout(0,1));
+		framePrincipal.add(grillePanel, BorderLayout.CENTER);
+		String cases[][] = {{"1", "", "", "", "", "", "", "", "", "", ""},
+							{"2", "", "", "", "", "", "", "", "", "", ""},
+							{"3", "", "", "", "", "", "", "", "", "", ""},
+							{"4", "", "", "", "", "", "", "", "", "", ""},
+							{"5", "", "", "", "", "", "", "", "", "", ""},
+							{"6", "", "", "", "", "", "", "", "", "", ""},
+							{"7", "", "", "", "", "", "", "", "", "", ""},
+							{"8", "", "", "", "", "", "", "", "", "", ""},
+							{"9", "", "", "", "", "", "", "", "", "", ""},
+							{"10", "", "", "", "", "", "", "", "", "", ""}};
+		String entetes[] = {"", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J"};
+		
+		JTable grilleAI = new JTable(cases, entetes);
+		JTable grilleJ = new JTable(cases, entetes);
+		grillePanel.add(grilleAI);
+		grillePanel.add(grilleJ);
+		
+		JPanel tirsPanel = new JPanel();
+		framePrincipal.add(tirsPanel, BorderLayout.LINE_END);
+		
+		
+		JPanel logPanel = new JPanel();
+		JTextField tbLog = new JTextField();
+		framePrincipal.add(tbLog, BorderLayout.PAGE_END);
+		
+		
+		framePrincipal.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		framePrincipal.setVisible(true);
+	}
+	
+	private static void addComponentToPane(Container pane) {
+		
+	}
+	
+	private static JButton addButton(String text, Container container) {
+		JButton button = new JButton(text);
+		button.setAlignmentX(Component.LEFT_ALIGNMENT);
+		return button;
 	}
 }
