efeb39c4ca9393adf406bbf8bb0003b3a5ff5f41
Defsv <defsv8@gmail.com>
2016-12-04 23:15:19 -0500

    Main a Nicolas

--- src/bn/Main.java
+++ src/bn/Main.java
@@ -7,6 +7,10 @@ import java.awt.*;
 public class Main {
 
 	private static final int MAX_GRID_SIZE = 11;
+    private static final int MIN_X = 0;
+    private static final int MAX_X = 9;
+    private static final int MIN_Y = 1;
+    private static final int MAX_Y = 10;
 
 	public static void main(String[] args) {
 		
@@ -19,21 +23,8 @@ public class Main {
 			
 		});
 		
-		
-			/*SwingUtilities.invokeLater(new Runnable() {
-			public void run() {
-			Calculator calc = new Calculator();
-			calc.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			calc.setVisible(true);
-			}
-			});*/
-		
-		
-		
 		JoueurArtificiel AI = new JoueurArtificiel(true);
 		JoueurHumain JH = new JoueurHumain();
-		
-
 		Navire navire = new Navire("Navire 3",3);
 		Navire navire2 = new Navire("Navire 5", 5);
 		Navire navire3 = new Navire("Navire 4",4);
@@ -60,109 +51,142 @@ public class Main {
 	}
 	
 	public static void demarre() {
-		JFrame framePrincipal = new JFrame("Bataille navalle");
-		framePrincipal.setBounds(200, 200, 800, 600);
-		framePrincipal.setLayout(new BorderLayout());
-				
-		JPanel buttonPanel = new JPanel();
-		buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.Y_AXIS));
-		framePrincipal.add(buttonPanel, BorderLayout.LINE_START);
-		buttonPanel.add(addButton("Placer Porte avion"));
-		buttonPanel.add(addButton("Placer Croiseur"));
-		buttonPanel.add(addButton("Placer Contre-T"));
-		buttonPanel.add(addButton("Placer Sous-Marin"));
-		buttonPanel.add(addButton("Placer Torpilleur"));
-		buttonPanel.add(addButton("Annuler"));
-		buttonPanel.add(addButton("Aide"));
-		buttonPanel.add(addButton("Recommencer"));
-		
-		JPanel grillePanel = new JPanel();
-		grillePanel.setLayout(new BoxLayout(grillePanel, BoxLayout.Y_AXIS));
-		framePrincipal.add(grillePanel, BorderLayout.CENTER);
-		Object cases[][] = {{"1", "", "", "", "", "", "", "", "", "", ""},
-							{"2", "", "", "", "", "", "", "", "", "", ""},
-							{"3", "", "", "", "", "", "", "", "", "", ""},
-							{"4", "", "", "", "", "", "", "", "", "", ""},
-							{"5", "", "", "", "", "", "", "", "", "", ""},
-							{"6", "", "", "", "", "", "", "", "", "", ""},
-							{"7", "", "", "", "", "", "", "", "", "", ""},
-							{"8", "", "", "", "", "", "", "", "", "", ""},
-							{"9", "", "", "", "", "", "", "", "", "", ""},
-							{"10", "", "", "", "", "", "", "", "", "", ""}};
-		
-		String entetes[] = {"", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J"};
+        JFrame framePrincipal = new JFrame("Bataille navalle");
+        framePrincipal.setBounds(200, 200, 800, 600);
+        framePrincipal.setLayout(new BorderLayout());
+
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.Y_AXIS));
+        framePrincipal.add(buttonPanel, BorderLayout.LINE_START);
+        buttonPanel.add(addButton("Placer Porte avion"));
+        buttonPanel.add(addButton("Placer Croiseur"));
+        buttonPanel.add(addButton("Placer Contre-T"));
+        buttonPanel.add(addButton("Placer Sous-Marin"));
+        buttonPanel.add(addButton("Placer Torpilleur"));
+        buttonPanel.add(addButton("Annuler"));
+        buttonPanel.add(addButton("Aide"));
+        buttonPanel.add(addButton("Recommencer"));
+
+        JPanel grillePanel = new JPanel();
+        grillePanel.setLayout(new BoxLayout(grillePanel, BoxLayout.Y_AXIS));
+        framePrincipal.add(grillePanel, BorderLayout.CENTER);
+        Object cases[][] = {{"1", "", "", "", "", "", "", "", "", "", ""},
+                {"2", "", "", "", "", "", "", "", "", "", ""},
+                {"3", "", "", "", "", "", "", "", "", "", ""},
+                {"4", "", "", "", "", "", "", "", "", "", ""},
+                {"5", "", "", "", "", "", "", "", "", "", ""},
+                {"6", "", "", "", "", "", "", "", "", "", ""},
+                {"7", "", "", "", "", "", "", "", "", "", ""},
+                {"8", "", "", "", "", "", "", "", "", "", ""},
+                {"9", "", "", "", "", "", "", "", "", "", ""},
+                {"10", "", "", "", "", "", "", "", "", "", ""}};
+        String entetes[] = {"", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J"};
 
         JTable grilleAI = new JTable(cases, entetes);
         JTable grilleJ = new JTable(cases, entetes);
-		for (int i = 0;i < MAX_GRID_SIZE-1; i++) {
-			for (int j = 1; j < MAX_GRID_SIZE; j++){
-				//grilleAI.isCellEditable(i, j);
-				//grilleAI.setEnabled(false);
-				//grilleJ.setEnabled(false);
-			}
-		}
-		JScrollPane scrollPaneAI = new JScrollPane(grilleAI);
-		JScrollPane scrollPaneJ = new JScrollPane(grilleJ);
-		grillePanel.add(new JLabel("Grille adverse"));
-		grillePanel.add(scrollPaneAI);
-		grillePanel.add(new JLabel("Votre grille"));
-		grillePanel.add(scrollPaneJ);
-		
-		JPanel tirsPanel = new JPanel();
-		framePrincipal.add(tirsPanel, BorderLayout.LINE_END);
-		tirsPanel.setLayout(new BoxLayout(tirsPanel, BoxLayout.Y_AXIS));
-		JLabel dernierTirsAI = new JLabel("5 Derniers tirs AI");
-		JLabel tirsAI1 = new JLabel("1");
-		JLabel tirsAI2 = new JLabel("2");
-		JLabel tirsAI3 = new JLabel("3");
-		JLabel tirsAI4 = new JLabel("4");
-		JLabel tirsAI5 = new JLabel("5");
-		tirsPanel.add(dernierTirsAI);
-		tirsPanel.add(tirsAI1);
-		tirsPanel.add(tirsAI2);
-		tirsPanel.add(tirsAI3);
-		tirsPanel.add(tirsAI4);
-		tirsPanel.add(tirsAI5);
 
-		JLabel dernierTirsJ = new JLabel("5 Vos derniers tirs");
-		JLabel tirsJ1 = new JLabel("1");
-		JLabel tirsJ2 = new JLabel("2");
-		JLabel tirsJ3 = new JLabel("3");
-		JLabel tirsJ4 = new JLabel("4");
-		JLabel tirsJ5 = new JLabel("5");
+        JScrollPane scrollPaneAI = new JScrollPane(grilleAI);
+        JScrollPane scrollPaneJ = new JScrollPane(grilleJ);
+        grillePanel.add(new JLabel("Grille adverse"));
+        grillePanel.add(scrollPaneAI);
+        grillePanel.add(new JLabel("Votre grille"));
+        grillePanel.add(scrollPaneJ);
+
+        JPanel tirsPanel = new JPanel();
+        framePrincipal.add(tirsPanel, BorderLayout.LINE_END);
+        tirsPanel.setLayout(new BoxLayout(tirsPanel, BoxLayout.Y_AXIS));
+        JLabel dernierTirsAI = new JLabel("5 Derniers tirs AI");
+        JLabel[] tirsAI = createLabels();
+        tirsPanel.add(dernierTirsAI);
+        for (int i = 0; i < tirsAI.length-1; i++) {
+            tirsPanel.add(tirsAI[i]);
+        }
+
+        JLabel dernierTirsJ = new JLabel("5 Vos derniers tirs");
+        JLabel[] tirsJ = createLabels();
         tirsPanel.add(dernierTirsJ);
-        tirsPanel.add(tirsJ1);
-        tirsPanel.add(tirsJ2);
-        tirsPanel.add(tirsJ3);
-        tirsPanel.add(tirsJ4);
-        tirsPanel.add(tirsJ5);
+        for (int i = 0; i < tirsJ.length-1; i++) {
+            tirsPanel.add(tirsJ[i]);
+        }
 
-		JPanel logPanel = new JPanel();
-		JTextField tbLog = new JTextField();
-		framePrincipal.add(tbLog, BorderLayout.PAGE_END);
-		
-		framePrincipal.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		framePrincipal.setVisible(true);
-	}
-	
-	private static void addComponentToPane(Container pane) {
-		
-	}
-	private static JButton addButton(String text) {
-		JButton button = new JButton(text);
-		return button;
+        JPanel logPanel = new JPanel();
+        JTextField tbLog = new JTextField();
+        framePrincipal.add(tbLog, BorderLayout.PAGE_END);
+
+        framePrincipal.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        framePrincipal.setVisible(true);
 	}
 
-	public static void placerNavire(String titre, int longueur) {
+    private static void addComponentToPane(Container pane) {
 
-	}
-	public static void annuler() {
+    }
 
-	}
-	public static void aide() {
+    private static JButton addButton(String text) {
+        JButton button = new JButton(text);
+        return button;
+    }
 
-	}
-	public static void recommencer() {
+    public static void placerNavire(String titre, int longueur) {
 
-	}
+    }
+
+    public static void annuler() {
+
+    }
+
+    public static void aide() {
+
+    }
+
+    public static void recommencer() {
+
+    }
+
+    /* Activer ou dÈsactiver un bouton
+
+    * button est un des boutons prÈsents sur le cÙtÈ gauche du panneaux
+    * status est un boolÈen qui dit si on active ou dÈsactive le bouton
+     */
+    public static void toggleButton(JButton button, boolean status) {
+        button.setEnabled(status);
+    }
+
+    /* …crit dans une des cases de la table donnÈe en paramËtre
+    *
+    * table est la grille d'un des joueurs
+    * x est la coordonnÈes x dans le tableau (doit Ítre entre 0 et 9)
+    * y est la coordonnÈes dans le tableau (doit Ítre entre 1 et 10)
+    * status est ce qui sera Ècrit dans la case
+     */
+    public static void setTextInTableCell(JTable table, int x, int y, String status) {
+        if ((x >= MIN_X) && (x <= MAX_X)) {
+            if ((y >= MIN_Y) && (y <= MAX_Y)) {
+                table.setValueAt(status, x, y);
+            }
+        }
+    }
+
+    public static void actuDerniersCoups(AJoueur j, JLabel[] tirs){
+        if (j instanceof JoueurHumain) { //Quand c'est le joueur humain
+            for (int i = 0; i < tirs.length; i++) {
+                actuLabelText(tirs[i], "");
+            }
+        } else if (j instanceof JoueurArtificiel) { //Quand c'est le joueur artificiel
+            for (int i = 0; i < tirs.length; i++) {
+                actuLabelText(tirs[i], "");
+            }
+        }
+    }
+
+    public static void actuLabelText(JLabel label, String action) {
+        label.setText(action);
+    }
+
+    public static JLabel[] createLabels() {
+        JLabel[] labels = new JLabel[4];
+        for (int i = 0; i < labels.length-1; i++) {
+            labels[i] = new JLabel("");
+        }
+        return labels;
+    }
 }
